7019fca529c523673f90ffecbbdba49bb1761f16
Никита Белый <mike.defo@mail.ru>
2025-06-18 23:59:53 +0300

    Добавлены все файлы, включая ClassLibrary

---
+++ ClassLibrary/DataUser.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace ClassLibrary
+{
+    public class DataUser
+    {
+
+        private Dictionary<string, List<DataObject>> AllDataUsers;
+        public DataUser(string pathFile)
+        {
+            if (File.Exists(pathFile))
+            {
+                var json = File.ReadAllText(pathFile);
+                AllDataUsers = JsonConvert.DeserializeObject<Dictionary<string, List<DataObject>>>(json) ?? new Dictionary<string, List<DataObject>>();
+            }
+            else
+            {
+                AllDataUsers = new Dictionary<string, List<DataObject>>();
+            }
+        }
+        public void AddData(string userName, string FIO, string Group, string Email, string Rating, string TransferDate, string TransferTime, string ScholarshipAvailability, string Photo, string Course)
+        {
+            try
+            {
+                if (!AllDataUsers.ContainsKey(userName))
+                {
+                    AllDataUsers.Add(userName, new List<DataObject>());
+                }
+                if (string.IsNullOrWhiteSpace(FIO))
+                {
+                    throw new Exception("Имя не может быть пустым.");
+                }
+                else if (string.IsNullOrWhiteSpace(Group))
+                {
+                    throw new Exception("Группа не может быть пустой.");
+                }
+                else if (string.IsNullOrWhiteSpace(Email))
+                {
+                    throw new Exception("Email не может быть пустым.");
+                }
+                else if (string.IsNullOrWhiteSpace(Convert.ToString(Rating)))
+                {
+                    throw new Exception("Рейтинг не может быть пустым.");
+                }
+                double NullD = 0;
+                if (!double.TryParse(Convert.ToString(Rating), out NullD))
+                {
+                    throw new Exception("Поле рейтинг должно содержать только число.");
+                }
+                if (!Email.Contains("@")) { throw new Exception("В поле Email отсутствует символ \"@\"."); }
+
+                if (AllDataUsers[userName].Count == 0)
+                {
+                    AllDataUsers[userName] = new List<DataObject>();
+                    AllDataUsers[userName].Add(new DataObject(FIO, Group, Email, Rating, TransferDate, TransferTime, ScholarshipAvailability, Course, Photo));
+                }
+                else { AllDataUsers[userName].Add(new DataObject(FIO, Group, Email, Rating, TransferDate, TransferTime, ScholarshipAvailability, Course, Photo)); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+        public void RemoveData(int indexToRemove, string userName)
+        {
+            try
+            {
+                if (!AllDataUsers.ContainsKey(userName))
+                {
+                    AllDataUsers.Add(userName, new List<DataObject>());
+                }
+                if (indexToRemove > 0 && indexToRemove <= AllDataUsers[userName].Count)
+                {
+                    AllDataUsers[userName].RemoveAt(indexToRemove - 1);
+                    Console.WriteLine($"Запись с номером {indexToRemove} удалена.");
+                }
+                else
+                {
+                    throw new Exception("Введен несуществующий номер записи.");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        public void AddUser(string name)
+        {
+            List<DataObject> dataObjects = new List<DataObject>();
+            AllDataUsers.Add(name, dataObjects);
+            SaveAll(name, dataObjects);
+        }
+        public List<DataObject> ProvideData(string userName)
+        {
+            try
+            {
+                return AllDataUsers[userName];
+            }
+            catch (Exception ex) { return null; }
+        }
+        public void SaveAll(string nameUser, List<DataObject> newDatas)
+        {
+            string pathFile = "User_Data.json";
+            try
+            {
+                AllDataUsers[nameUser] = newDatas;
+                // Сериализуем словарь в строку JSON
+                string json = JsonConvert.SerializeObject(AllDataUsers, Formatting.Indented);
+                // Записываем строку JSON в файл
+                File.WriteAllText(pathFile, json);
+            }
+            catch { return; }
+        }
+    }
+
+    public class DataObject
+    {
+        public string FIO;
+        public string Group;
+        public string Email;
+        public string Rating;
+        public string TransferDate;
+        public string TransferTime;
+        public string ScholarshipAvailability;
+        public string Photo;
+        public string Course;
+        public DataObject(string FIO, string Group, string Email, string Rating, string TransferDate, string TransferTime, string ScholarshipAvailability, string Course, string Photo)
+        {
+            this.FIO = FIO;
+            this.Group = Group;
+            this.Email = Email;
+            this.Rating = Rating;
+            this.TransferDate = TransferDate;
+            this.TransferTime = TransferTime;
+            this.ScholarshipAvailability = ScholarshipAvailability;
+            this.Photo = Photo;
+            this.Course = Course;
+        }
+        public DataObject() {; }
+    }
+}
---
+++ ClassLibrary/UserManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using static System.Net.WebRequestMethods;
+
+namespace ClassLibrary
+{
+    public class UserManager
+    {
+        string pathFile;
+        private Dictionary<string, User> AllUsers;
+        public UserManager(string pathFile)
+        {
+            this.pathFile = pathFile;
+            if (System.IO.File.Exists(pathFile))
+            {
+                var json = System.IO.File.ReadAllText(pathFile);
+                AllUsers = JsonConvert.DeserializeObject<Dictionary<string, User>>(json) ?? new Dictionary<string, User>();
+            }
+            else
+            {
+                AllUsers = new Dictionary<string, User>();
+            }
+        }
+        public bool AddUser(string username, string password)
+        {
+            try
+            {
+                if (AllUsers.ContainsKey(username))
+                {
+                    return false;
+                }
+                User temp = new User(password);
+
+                AllUsers.Add(username, temp);
+                SaveAll(pathFile);
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+        public bool Authorization(string username, string password)
+        {
+            try
+            {
+                if (username == null || password == null)
+                {
+                    return false;
+                }
+                if (!AllUsers.ContainsKey(username))
+                {
+                    throw new Exception("Нет такого пользователя.");
+                }
+                User value = AllUsers[username];
+                if (value.Password != password)
+                {
+                    throw new Exception("Введен не верный пароль.");
+                }
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+        public void SaveAll(string pathFile)
+        {
+            try
+            {
+                // Сериализуем словарь в строку JSON
+                string json = JsonConvert.SerializeObject(AllUsers, Newtonsoft.Json.Formatting.Indented);
+                // Записываем строку JSON в файл
+                System.IO.File.WriteAllText(pathFile, json);
+            }
+            catch {; }
+        }
+    }
+    public class User
+    {
+        public string Password;
+        public User(string Password)
+        {
+            this.Password = Password;
+        }
+
+    }
+}
